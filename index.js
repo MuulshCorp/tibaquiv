ee399bc538cefddfd8983271bbf03dfccee98bc5
MuulshCorp <38662305+MuulshCorp@users.noreply.github.com>
2018-05-01 12:59:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ else if (message.content === 'Nan')             { message.channel.send      ('..
 else if (message.content === 'M. Tib aquiv')    { message.channel.send      ('yo la stutt ') }
 else if (message.content === 'Timeur2.0')       { message.channel.send      ('le grand dieu est arrivé sur Terre nous devons le vénéré et lui donné des cadeaux ') }
 else if (message.content === 'Müulsh')          { message.channel.send      ('le dieu de la méchanceté est arrivé attention il va vous trollé (attention au mecs qui n\'ont pas d\'humour lol) sinon c\'est tranche de rire à volonté .') }
-else if (message.content === '#dieuxestla')     { message.channel.send      ('Ah tiens on ma reconnu merci {user}') }
+else if (message.content === '#dieuxestla')     { message.channel.send      ('Ah tiens on ma reconnu merci .') }
 })
 
 
