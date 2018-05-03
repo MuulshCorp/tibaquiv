86ada30c15152040fe3bfad3ebe686cabef80bd6
MuulshCorp <38662305+MuulshCorp@users.noreply.github.com>
2018-05-03 06:54:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ else if (message.content === '.boss')           { message.channel.send      ('c\
 else if (message.content === '.maj')            { message.channel.send      ('un jour ou l\'autre j\'aurai une mise à jour :sob:') }
 else if (message.content === '.credit')         { message.channel.send      ('bot créé par <@!385337893553438720> avec l\'aide de <@!317968962656796674> car il a une grosse b... heu moi ?! nan JAMAIS ! (désolé mes créateur sont des hommes ils ont oublier le féminin c\'est des connards et ils sont vulgaires et font des fautes d\'orthographe)') }
 else if (message.content === '.id')             { message.channel.send      ('salut moi je suis M. tib aquiv un bot de afk  super utile') }  
-else if (message.content === '.dico')           { message.channel.send      ('indisponible pour le moment #dico (et la chose que tu veux savoir ) = un lien vers la page web#') }
+else if (message.content.startsWith('.dico'))   { message.channel.send      ('indisponible pour le moment #dico (et la chose que tu veux savoir ) = un lien vers la page web#') }
 
 //secret de la commande dico:
 else if (message.content === '.dico caca')      { message.channel.send      ('t\'es fou toi!? chu pas dégueu  va le faire toi même (avec une image de mec qui caque sur ces chiottes)') }
